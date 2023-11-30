487e512f21202d13a04c0804ffb567ad7a29bd20
cheeesecake77 <vasya1679@mail.ru>
2023-11-30 21:37:58 +0300

    second day

---
+++ Day2/variables.py
@@ -0,0 +1,54 @@
+# Day 2: 30 days of Python programming
+first_name = 'sample name'
+last_name = "sample_name"
+full_name = first_name + " " + last_name
+country = 'Hell'
+city = 'Nine circle'
+age = 54
+year = 2023
+is_married, is_true, is_light = False, False, True
+
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_light))
+print(type(is_true))
+
+print(len(first_name))
+
+num_one = 5
+num_two = 4
+
+total = num_one + num_two
+diff = num_one + num_two
+product = num_one * num_two
+division = num_one / num_two
+remaider = num_two % num_one
+exp = num_one ** num_two
+floor_division = num_one // num_two
+
+radius = 30
+area_of_circle = radius ** 2 * 3.14
+circumference_of_circle = radius * 2 * 3.14
+
+radius = input()
+area_of_circle = radius ** 2 * 3.14
+
+fname = input()
+lname = input()
+country_ = input()
+age_ = input()
+
+
+
+
+
+
+
+
+
